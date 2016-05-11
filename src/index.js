4451bef86c3ecf0d15190dc504d405d20e023ab3
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-05-11 16:23:39 +0200

    chore(lib): attempts at 'library' exports using es6 syntax - added main index as entry point, re-exporting the bits and pieces of 'sub library' - updated package to point to the index.js above

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as diffPatchUtils from './diffPatchUtils'
+import * as modelUtils from './modelUtils'
+import * as obsUtils from './modelUtils'
+import * as utils from './utils'
+
+export {diffPatchUtils, modelUtils, obsUtils, utils }
