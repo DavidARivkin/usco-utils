87968c53cc6de66752f47c2fefb830a9831dbac2
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-05-12 15:11:45 +0200

    feat(move to ava testing): updated specs & package

---
+++ src/diffPatchUtils.spec.js
@@ -0,0 +1,37 @@
+import test from 'ava'
+import 'babel-core/register'
+import { extractChangesBetweenArrays } from './diffPatchUtils'
+
+test.cb('diffPatchUtils: extractChangesBetweenArrays: should determine if there was a addition', t => {
+  const previous = [{name: 'foo'}]
+  const current = [{name: 'foo'}, {name: 'bar'}]
+
+  const changes = extractChangesBetweenArrays(previous, current)
+  const expChanges = {'added': [{name: 'bar'}], 'removed': [], 'changed': [], 'upserted': []}
+
+  console.log('changes', changes)
+  t.deepEqual(changes, expChanges)
+  t.end()
+})
+
+test.cb('diffPatchUtils: extractChangesBetweenArrays: should determine if there was a removal', t => {
+  const previous = [{name: 'foo'}, {name: 'bar'}]
+  const current = [{name: 'foo'}]
+
+  const changes = extractChangesBetweenArrays(previous, current)
+  const expChanges = {'added': [], 'removed': [{name: 'bar'}], 'changed': [], 'upserted': []}
+
+  t.deepEqual(changes, expChanges)
+  t.end()
+})
+
+/* it("should determine if there was an update", function() {
+  const previous = [{name:'foo',id:0},{name:'bar',id:1}]
+  const current  = [{name:'foobar',id:0},{name:'bar',id:1}]
+
+  const changes  = extractChangesBetweenArrays(previous, current)
+  const expChanges = {'added':[],'removed':[],'changed':[],'upserted':[{name:'foobar',id:0}]}
+
+  console.log("changes",changes)
+  assert.strictEqual(changes, expChanges)
+})*/
--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,25 +1,23 @@
-import assert from 'assert'
+import test from 'ava'
+import 'babel-core/register'
 import { isEmpty, getNameAndExtension } from './utils'
 
-describe('utils', function () {
-  describe('isEmpty', () => {
-    it('should determine if a string is empty', function () {
-      const emptyInput = ''
-      const notEmptyInput = 'foo'
-      assert.strictEqual(isEmpty(emptyInput), true)
-      assert.strictEqual(isEmpty(notEmptyInput), false)
-    })
+test.cb('utils: isEmpty: should determine if a string is empty', t => {
+  const emptyInput = ''
+  const notEmptyInput = 'foo'
+  t.deepEqual(isEmpty(emptyInput), true)
+  t.deepEqual(isEmpty(notEmptyInput), false)
+  t.end()
+})
 
-    it('should not fail with a non string input ', function () {
-      const input = {foo: 42}
-      assert.strictEqual(isEmpty(input), false)
-    })
-  })
+test.cb('utils: isEmpty: should not fail with a non string input', t => {
+  const input = {foo: 42}
+  t.deepEqual(isEmpty(input), false)
+  t.end()
+})
 
-  describe('getNameAndExtension', () => {
-    it('should return the name and the extension from a dotted string', function () {
-      const input = 'foo.bar.STL'
-      assert.deepEqual(getNameAndExtension(input), {name: 'foo.bar.STL', ext: 'stl'})
-    })
-  })
+test.cb('utils: getNameAndExtension: should return the name and the extension from a dotted string', t => {
+  const input = 'foo.bar.STL'
+  t.deepEqual(getNameAndExtension(input), {name: 'foo.bar.STL', ext: 'stl'})
+  t.end()
 })
