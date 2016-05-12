67a1084e9c1c6a1c2abe721c3f8f0a918f313022
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-05-12 08:48:17 +0200

    feat(entry points): added 'root' level entry points to sub libraries

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as diffPatchUtils from './diffPatchUtils'
 import * as modelUtils from './modelUtils'
-import * as obsUtils from './modelUtils'
+import * as obsUtils from './obsUtils'
 import * as utils from './utils'
 
 export {diffPatchUtils, modelUtils, obsUtils, utils }
